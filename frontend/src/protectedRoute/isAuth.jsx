49fa297a20a0d88af716cbf12e087101f9b1995e
abdenacer <nasseressaouira@gmail.com>
2023-01-16 06:59:27 +0800

    j'ai fini le projet de g-formation avec le protege des router et creer docker-compose

--- frontend/src/protectedRoute/isAuth.jsx
+++ frontend/src/protectedRoute/isAuth.jsx
@@ -1,5 +1,3 @@
-
-
 export const isAuth = () => {
 
   const role = localStorage.getItem('role')
--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -4,7 +4,6 @@ const baseURL = "http://localhost:8088/api/auth";
 
 export const authLogin = (email, password) => {
     return axios.post(`${baseURL}/login`, { email, password })
-
 };
 
 export const authLogout = () => {
